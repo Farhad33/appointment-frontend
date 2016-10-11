cd4a4f2f0fedf19bb76cca696bc28af4e510c421
Isis <imoran@mail.ccsf.edu>
2016-10-11 14:13:25 -0700

    starting services page

--- src/ServiceBox.js
+++ src/ServiceBox.js
@@ -1,22 +1,32 @@
 import React, { Component } from 'react';
 
 class ServiceBox extends Component {
-  componentWillMount() {
-    const options = {
-      mode: 'cors',
-
-    }
-    var url = `http://localhost:4000/services`
-    fetch(url, options)
-      .then(results => {
-        console.log(results)
-      })
-  }
+  // componentWillMount() {
+  //   const options = {
+  //     mode: 'cors',
+  //
+  //   }
+  //   var url = `http://localhost:4000/services`
+  //   fetch(url, options)
+  //     .then(results => {
+  //       console.log(results)
+  //     })
+  // }
   render() {
     return (
-      <div className="ServiceBox">
-        <h1>wat</h1>
-
+      <div className="ServiceBox-wrapper">
+        <div className="box-wrapper">
+          <div className="box"><h1>wat</h1></div>
+          <div className="box"><h1>wat</h1></div>
+          <div className="box"><h1>wat</h1></div>
+          <div className="box"><h1>wat</h1></div>
+          <div className="box"><h1>wat</h1></div>
+          <div className="box"><h1>wat</h1></div>
+          <div className="box"><h1>wat</h1></div>
+          <div className="box"><h1>wat</h1></div>
+          <div className="box"><h1>wat</h1></div>
+          <div className="box"><h1>wat</h1></div>
+        </div>
       </div>
     );
   }

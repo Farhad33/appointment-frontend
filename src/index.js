32fe26651b5d6db2a9f3c573476d4ed20e358852
Devon Wesley <dwesley89@gmail.com>
2016-10-11 14:26:42 -0700

    structured schedule folder, and data added to days file

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import ReactDOM from 'react-dom';
 import { Router, Route, browserHistory } from 'react-router'
 
 import Services from './Services'
+import Schedule from './scheduler/Schedule'
 
 ReactDOM.render((
   <Router history={ browserHistory }>
     <Route path="/" component={ Services } />
+    <Route path="/schedule" component={ Schedule } />
   </Router>),
   document.getElementById( 'root' )
 );

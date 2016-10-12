0226c23af62f15f85b874a3bb0add99aa4ff6422
Aileen Santos <asantos3026@gmail.com>
2016-10-11 17:55:34 -0700

    bug fixes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import Services from './Services'
-import Schedule from './scheduler/Schedule'
 
 ReactDOM.render(
   <App />,
